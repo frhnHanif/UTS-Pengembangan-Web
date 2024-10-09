e8d0ecc028b9937d1366316606d200314303e58a
Muhammad Afiq Ramadhan Oktaviyanto <muhammadafiqramadhan@students.undip.ac.id>
2024-10-09 15:07:57 +0700

    Update db.sql

--- database/db.sql
+++ database/db.sql
@@ -8,6 +8,7 @@ CREATE TABLE customer (
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     address VARCHAR(100) NOT NULL,
+    angkatan INT(4) NOT NULL,
     phone VARCHAR(15)
 );
 
